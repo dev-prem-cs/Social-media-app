83c8cb6bda5c698631eb28b9e4c8ba3344a89489
dev-prem-cs <premcs.exe@gmail.com>
2025-10-22 14:16:20 +0530

    feat(auth): enhance error handling and add debug logs in authentication middleware

--- backend/user/src/middleware/auth.middleware.ts
+++ backend/user/src/middleware/auth.middleware.ts
@@ -14,16 +14,24 @@ export const isAuth= async(req:AuthenticatedRequest,res:Response,next:NextFuncti
                 message:"Please login , no auth header found!!"
             })
         }
+        console.log("mere andar aa gaya")
         const token :any =authHeaders!.split(" ")[1];
         const decodedToken = jwt.verify(token , process.env.JWT_SECRET as string) as JwtPayload;
+        console.log("verify hoo gaya")
         if (!decodedToken || !decodedToken.user) {
             res.status(401).json({ message: "Invalid token. Please login again." });
             return;
         }
         req.user = decodedToken.user;
+        console.log("yahoooo")
+
         next();
 
-    } catch (error) {
-        
+    } catch (error : any) {
+        res.status(401).json({ message: "Unauthorized access. Please login.",
+            err:error.message 
+
+         },
+        );
     }
 }
